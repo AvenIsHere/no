afd6ca64aac4532778a680adfd21427cb31d8b35
Aven <a@avenf.com>
2026-05-28 15:48:42 +0100

    added ability to change output

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-[[noreturn]] int main() {
-    while (1) {
-        printf("n\n");
+
+[[noreturn]] int main(const int argc, char** argv) {
+
+    char* out = "n";
+
+    if (argc > 1) {
+        out = argv[1];
     }
+
+    while (1) {
+        printf("%s\n", out);
+    }
+
 }
